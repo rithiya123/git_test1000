cb89fda24267960a3349769531828c0ef94313e6
mac <rathya04@gmail.com>
2022-08-17 11:06:43 +0700

    Initial Commit

---
+++ git_test1000/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git_test1000
+//
+//  Created by YaaBee on 17/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ git_test1000UITests/git_test1000UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  git_test1000UITestsLaunchTests.swift
+//  git_test1000UITests
+//
+//  Created by YaaBee on 17/8/22.
+//
+
+import XCTest
+
+class git_test1000UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
